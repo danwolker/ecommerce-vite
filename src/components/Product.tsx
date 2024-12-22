245ef9d66f8eb5ab46b9c67097e3b85f2b8b187b
DanIglessias <danewolker@gmail.com>
2024-12-21 23:42:20 -0300

    Implementação Rate

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,26 +3,43 @@ import { faCartShopping, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
-export default function Product({ id, image, name, rate, price }){
-   return (
+interface ProductProps {
+  id: number;
+  name: string;
+  image: string;
+  rating: number; // Corrigido para usar o nome correto
+  price: number;
+}
+
+const Product: React.FC<ProductProps> = ({ id, name, image, rating, price }) => {
+  return (
     <div className="product">
-      <img src={image} alt={name}
-      />
+      <img src={image} alt={name} />
       <p className="name">{name}</p>
-      <p className="rate">{"⭐".repeat(rate)}</p>
+
+      {/* Renderização dinâmica do rating */}
+      <p className="rate">
+        {Array.from({ length: rating }, (_, index) => (
+          <span key={index}>&#9733;</span>
+        ))}
+      </p>
+
       <p className="price">
         <span>R$</span> {price}
       </p>
+
       <div className="buttons">
         <Link to={`/products/${id}/checkout`} className="btn-icon">
           <span>Comprar Agora</span>
           <FontAwesomeIcon icon={faMoneyBill} />
         </Link>
-        <button className="btn icon add-icon-cart-btn">
+        <button className="btn-icon add-to-cart-btn">
           <span>Adicionar ao carrinho</span>
           <FontAwesomeIcon icon={faCartShopping} />
         </button>
       </div>
     </div>
   );
-}
+};
+
+export default Product;
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Product from "./Product";
 
-export default function ProductsList({ products }) {
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string; // Caminho ou URL da imagem
+  rating: number; // Corrigido para refletir o JSON
+}
+
+interface ProductsListProps {
+  products: Product[];
+}
+
+export default function ProductsList({ products }: ProductsListProps) {
   return (
     <div className="product-list">
       {products.map((product) => (
         <Product key={product.id} {...product} />
-
       ))}
     </div>
   );
